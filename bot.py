7fc3df595d1aeee0a12e1a22c7502bc027f93b3c
Fepe <fepe55@gmail.com>
2021-04-21 19:04:44 -0300

    New allowed sticker set

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 API_TOKEN = os.getenv('API_TOKEN')
 DEBUG = os.getenv('SZERDA_DEBUG', False)
 
-ALLOWED_STICKER_SETS = ['Piggy2019', 'vinki', 'NickWallowPig']
+ALLOWED_STICKER_SETS = ['Piggy2019', 'vinki', 'NickWallowPig', 'waddles_pig']
 # FILE_PATH = 'resultados.json'
 
 
